952c264886a036e4450f394744e82a3f7ec0f716
gekal <liu.hongying@hotmail.com>
2026-03-05 20:12:03 +0900

    Replace `IllegalArgumentException` with `UserNotFoundException` in `UserService` and related components. Add centralized exception handling in `UserApi`.

--- app/src/main/java/cn/gekal/spring/template/presentation/RestControllerAdvice.java
+++ app/src/main/java/cn/gekal/spring/template/presentation/RestControllerAdvice.java
@@ -1,6 +1,16 @@
 package cn.gekal.spring.template.presentation;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class RestControllerAdvice {}
+public class RestControllerAdvice {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleGenericException(Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("An unexpected error occurred: " + ex.getMessage());
+  }
+}
