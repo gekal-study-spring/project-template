9f33a0fcfd42c703deff85fd7f163e3dee74581d
gekal <liu.hongying@hotmail.com>
2026-03-09 22:51:37 +0900

    Handle `AuthorizationDeniedException` and update actuator visibility
    - Add exception handler for `AuthorizationDeniedException` in `RestControllerAdvice`.
    - Enable actuator display through `application.yaml` configuration.

--- app/src/main/java/cn/gekal/spring/template/presentation/RestControllerAdvice.java
+++ app/src/main/java/cn/gekal/spring/template/presentation/RestControllerAdvice.java
@@ -2,11 +2,18 @@ package cn.gekal.spring.template.presentation;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RestControllerAdvice {
+
+  @ExceptionHandler(AuthorizationDeniedException.class)
+  public ResponseEntity<String> handleAuthorizationDeniedException(
+      AuthorizationDeniedException ex) {
+    throw ex;
+  }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleGenericException(Exception ex) {
