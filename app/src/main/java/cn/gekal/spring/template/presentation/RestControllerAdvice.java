b61d1de12664300959d4b652997b50b6abe4f3e2
gekal <liu.hongying@hotmail.com>
2026-03-11 22:32:12 +0900

    Add logging for exceptions in `RestControllerAdvice`

--- app/src/main/java/cn/gekal/spring/template/presentation/RestControllerAdvice.java
+++ app/src/main/java/cn/gekal/spring/template/presentation/RestControllerAdvice.java
@@ -1,5 +1,7 @@
 package cn.gekal.spring.template.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
@@ -11,19 +13,24 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 @ControllerAdvice
 public class RestControllerAdvice {
 
+  private static final Logger log = LoggerFactory.getLogger(RestControllerAdvice.class);
+
   @ExceptionHandler(AuthorizationDeniedException.class)
   public ResponseEntity<String> handleAuthorizationDeniedException(
       AuthorizationDeniedException ex) {
-    throw ex;
+    log.error("Authorization denied: ", ex);
+    return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
   }
 
   @ExceptionHandler({NoHandlerFoundException.class, NoResourceFoundException.class})
   public ResponseEntity<String> handleNotFoundException(Exception ex) {
+    log.error("Resource not found: ", ex);
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleGenericException(Exception ex) {
+    log.error("An unexpected error occurred: ", ex);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body("An unexpected error occurred: " + ex.getMessage());
   }
